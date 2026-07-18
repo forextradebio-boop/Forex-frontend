d5c6dd7952bcc6ea41e61918194dde6f9e2d1335
forextradebio-boop <forextradebio@gmail.com>
2026-07-18 16:20:13 +0530

    Fix API endpoint for getting payment settings

--- src/services/paymentSettings.ts
+++ src/services/paymentSettings.ts
@@ -1,11 +1,11 @@
 import api from '../api/axios';
 
 export const getPaymentSettings = async () => {
-  const res = await api.get('/api/payment-settings');
+  const res = await api.get('/payment-settings');
   return res.data;
 };
 
 export const updatePaymentSettings = async (payload: any) => {
-  const res = await api.patch('/api/payment-settings', payload);
+  const res = await api.patch('/payment-settings', payload);
   return res.data;
 };
