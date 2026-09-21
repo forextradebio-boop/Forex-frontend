ece8d267cebfdb3c0e15aba7e28885bf3c95d62c
forextradebio-boop <forextradebio@gmail.com>
2026-09-21 14:14:03 +0530

    Update config.ts

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,5 +1,5 @@
-const LIVE_API_BASE_URL = "https://forex-backend-iem1.onrender.com";
-const LIVE_SOCKET_URL = "https://forex-backend-iem1.onrender.com";
+const LIVE_API_BASE_URL = "https://forex-backend-cils.onrender.com";
+const LIVE_SOCKET_URL = "https://forex-backend-cils.onrender.com";
 
 export const API_BASE_URL = LIVE_API_BASE_URL;
 export const SOCKET_URL = LIVE_SOCKET_URL;
